ea0f40e26d21f400c9f4b64374816267c436e3bc
cocomongg <pjm4142@naver.com>
2025-03-25 01:31:08 +0900

    feat: #19 DeleteNoticeUseCase 추가

--- src/main/java/com/rsupport/notice/application/notice/usecase/DeleteNoticeUseCase.java
+++ src/main/java/com/rsupport/notice/application/notice/usecase/DeleteNoticeUseCase.java
@@ -1,6 +1,11 @@
 package com.rsupport.notice.application.notice.usecase;
 
+import com.rsupport.notice.domain.file.service.StorageService;
+import com.rsupport.notice.domain.notice.entity.NoticeFile;
+import com.rsupport.notice.domain.notice.service.NoticeFileService;
 import com.rsupport.notice.domain.notice.service.NoticeService;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,9 +15,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class DeleteNoticeUseCase {
 
     private final NoticeService noticeService;
+    private final NoticeFileService noticeFileService;
+    private final StorageService storageService;
 
     @Transactional
-    public void execute(Long noticeId) {
-        noticeService.deleteNotice(noticeId);
+    public void execute(Long noticeId, LocalDateTime now) {
+        noticeService.deleteNotice(noticeId, now);
+
+        List<NoticeFile> deletedNoticeFileList = noticeFileService.getNoticeFileList(noticeId);
+        noticeFileService.deleteNoticeFiles(noticeId, now);
+
+        for (NoticeFile noticeFile : deletedNoticeFileList) {
+            String fileFullPath = noticeFile.getFileFullPath();
+            storageService.remove(fileFullPath);
+        }
     }
 }
