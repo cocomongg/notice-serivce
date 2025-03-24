3fdd79da6747a17fc0b1e2cd79463d743793a724
cocomongg <pjm4142@naver.com>
2025-03-25 01:27:07 +0900

    test: #19 noticeFile 삭제 기능 통합테스트 추가

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -3,8 +3,8 @@ create table users
     user_id    bigint auto_increment comment 'user PK'
         primary key,
     username   varchar(20) null comment '사용자 이름',
-    created_at datetime    not null comment '생성일시',
-    updated_at datetime    null comment '수정일시'
+    created_at datetime(6)    not null comment '생성일시',
+    updated_at datetime(6)    null comment '수정일시'
 )
     comment '사용자';
 
@@ -18,9 +18,9 @@ create table notices
     notice_start_at datetime      not null comment '공지 시작일시',
     notice_end_at   datetime      not null comment '공지 종료일시',
     view_count      int default 0 null comment '공지사항 조회 수',
-    created_at      datetime      not null comment '생성일시',
-    updated_at      datetime      null comment '수정일시',
-    deleted_at      datetime      null comment '삭제일시'
+    created_at      datetime(6)     not null comment '생성일시',
+    updated_at      datetime(6)      null comment '수정일시',
+    deleted_at      datetime(6)      null comment '삭제일시'
 )
     comment '공지사항';
 
@@ -37,8 +37,8 @@ create table notice_files
     upload_file_name   varchar(255) not null comment '첨부파일 업로드 이름',
     file_path          varchar(255) not null comment '첨부파일 경로',
     file_size          int          not null comment '첨부파일 크기',
-    created_at         datetime     not null comment '생성일시',
-    deleted_at         datetime     null comment '삭제일시'
+    created_at         datetime(6)     not null comment '생성일시',
+    deleted_at         datetime(6)     null comment '삭제일시'
 )
     comment '공지사항 첨부파일';
 
